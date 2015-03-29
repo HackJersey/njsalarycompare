1168b3c07e0d9a9c22d58ff0bc71d59a8bf8b99a
Bill Lugo <lugow1@mail.montclair.edu>
2015-03-29 10:40:39 -0400

    API data pull - salaries of state employees by county then by pension fund

---
+++ pensionSalaries.js
@@ -0,0 +1,351 @@
+[ {
+  "avg_all_employers_salary_amt" : "39796.831861249398",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "ATLANTIC COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "88798.978877887789",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "ATLANTIC COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "69051.694630872483",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "ATLANTIC COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "46345.012351223894",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "BERGEN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "116328.28335093277",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "BERGEN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "74491.006860196418",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "BERGEN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "38431.161093381435",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "BURLINGTON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "82998.850510677809",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "BURLINGTON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "69693.533183352081",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "BURLINGTON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "41961.139302884615",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "CAMDEN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "81692.85060362173",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "CAMDEN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "67077.362060486845",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "CAMDEN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "42818.184546395309",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "CAPE MAY COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "82989.37330754352",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "CAPE MAY COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "70619.497256515775",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "CAPE MAY COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "39252.148984198646",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "CUMBERLAND COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "71645.03619047619",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "CUMBERLAND COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "64289.340530973451",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "CUMBERLAND COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "46218.938362405624",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "ESSEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "97119.726728723404",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "ESSEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "73432.618051313378",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "ESSEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "38743.00401676563",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "GLOUCESTER COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "86092.722222222222",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "GLOUCESTER COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "65130.733458568149",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "GLOUCESTER COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "42304.992383243135",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "HUDSON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "97683.19966442953",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "HUDSON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "74028.788566573613",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "HUDSON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "42060.636448163645",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "HUNTERDON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "85069.07619047619",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "HUNTERDON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "66031.604442545921",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "HUNTERDON COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "47901.165437788018",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "MERCER COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "94687.166314305849",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "MERCER COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "71385.582024724357",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "MERCER COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "42883.841891122142",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "MIDDLESEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "100503.573770491803",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "MIDDLESEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "70766.547742981483",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "MIDDLESEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "43086.577849264706",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "MONMOUTH COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "102169.648535564854",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "MONMOUTH COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "69647.378531073446",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "MONMOUTH COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "44463.25377461046",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "MORRIS COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "98690.23646071701",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "MORRIS COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "71289.605712860172",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "MORRIS COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "41392.818312757202",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "OCEAN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "98488.494043447793",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "OCEAN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "64684.521770369306",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "OCEAN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "43944.215880597015",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "PASSAIC COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "100984.041485769416",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "PASSAIC COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "70408.500358851675",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "PASSAIC COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "55292.476635514019",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "REGIONAL"
+}
+, {
+  "avg_all_employers_salary_amt" : "80459",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "REGIONAL"
+}
+, {
+  "avg_all_employers_salary_amt" : "61808.727272727273",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "REGIONAL"
+}
+, {
+  "avg_all_employers_salary_amt" : "38523.02802359882",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "SALEM COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "74699.012295081967",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "SALEM COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "62979.037751004016",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "SALEM COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "44767.9945303659",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "SOMERSET COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "100958.120204603581",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "SOMERSET COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "68499.308087064236",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "SOMERSET COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "61261.072599468056",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "STATEWIDE"
+}
+, {
+  "avg_all_employers_salary_amt" : "73546.578111337802",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "STATEWIDE"
+}
+, {
+  "avg_all_employers_salary_amt" : "85870.836243694218",
+  "pension_fund_name" : "STATE POLICE RETIREMENT SYSTEM",
+  "location_name" : "STATEWIDE"
+}
+, {
+  "avg_all_employers_salary_amt" : "95466.711267605634",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "STATEWIDE"
+}
+, {
+  "avg_all_employers_salary_amt" : "40664.729648671808",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "SUSSEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "95299.610738255034",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "SUSSEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "72303.228041237113",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "SUSSEX COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "46671.242550464595",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "UNION COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "97344.666407465008",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "UNION COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "71994.551536874118",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "UNION COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "41488.965308370044",
+  "pension_fund_name" : "PUBLIC EMPLOYEES' RETIREMENT SYSTEM",
+  "location_name" : "WARREN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "82875.314410480349",
+  "pension_fund_name" : "POLICE AND FIREMEN'S RETIREMENT SYSTEM",
+  "location_name" : "WARREN COUNTY"
+}
+, {
+  "avg_all_employers_salary_amt" : "65850.88924218336",
+  "pension_fund_name" : "TEACHERS' PENSION AND ANNUITY FUND",
+  "location_name" : "WARREN COUNTY"
+}
+ ]
